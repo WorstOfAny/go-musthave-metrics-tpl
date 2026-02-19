ee54bfc26123064c5f3c4ec8a0608929097de415
Ivan Tyurin <i.tyurin@voskhod.ru>
2026-02-20 01:21:35 +0300

    Инкремент 2
    This reverts commit 0a9092ba2fb422cecc5b7a7cb2cfbf02e6a81220.

--- internal/handler/main.go
+++ internal/handler/main.go
@@ -1,30 +1,22 @@
 package handler
 
 import(
-	"internal/logger"
+	"github.com/WorstOfAny/go-musthave-metrics-tpl/internal/logger"
+	"github.com/WorstOfAny/go-musthave-metrics-tpl/internal/model"
 	"net/http"
 	"strings"
 )
-type Setter interface { Set(name string, value string) error }
-type Getter interface { Get(name string) (any, bool) }
-type Remover interface { Remove(name string) }
-type Repository interface {
-	Setter
-	Getter
-	Remover
-}
 
 type controller struct {
 	l logger.Logger
-	repositories map[string]Repository
 }
 
 func (c *controller) ApplyTo(mux *http.ServeMux) {
 	mux.Handle("/update/{type}/{varName}/{varValue}", http.HandlerFunc(c.Update))
 }
 
-func NewController(l logger.Logger, repositories map[string]Repository) controller {
-	return controller{l: l, repositories: repositories}
+func NewController(l logger.Logger) controller {
+	return controller{l: l}
 }
 
 func (c *controller) Update(w http.ResponseWriter, r *http.Request) {
@@ -34,24 +26,21 @@ func (c *controller) Update(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if !strings.HasPrefix(r.Header.Get("Content-Type"), "text/plain") {
-		http.Error(w, "", http.StatusUnsupportedMediaType)
+		w.WriteHeader(http.StatusUnsupportedMediaType)
 		return
 	}
 
-	metric_type := r.PathValue("type")
-	metric_name := r.PathValue("varName")
-	metric_value := r.PathValue("varValue")
+	metric, ok := models.FindMetric(r.PathValue("type"), r.PathValue("varName"))
+	newMetric := !ok
 
-	c.l.Log(r.URL)
-	c.l.Log(metric_type)
-	c.l.Log(metric_name)
-	c.l.Log(metric_value)
+	if !ok {
+		metric, ok = models.NewMetric(r.PathValue("type"), r.PathValue("varName"))
+	}
 
-	if repo, ok := c.repositories[metric_type]; ok {
-		 err := repo.Set(metric_name, metric_value)
-		 if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-		 }
+	if ok && metric.Update(r.PathValue("varValue")) {
+		if newMetric {
+			metric.Save()
+		}
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
 	}
--- internal/storage/main_test.go
+++ internal/storage/main_test.go
@@ -1,238 +1,110 @@
 package storage
 
 import(
-	"internal/logger"
 	"testing"
 	"github.com/stretchr/testify/assert"
+
 )
 
 func TestNewStorage (t *testing.T) {
-	t.Run("Create Gauge Storage", func(t *testing.T) {
-		s := NewStorage[Gauge](nil)
+	t.Run("Create int64 Storage", func(t *testing.T) {
+		s := NewStorage[*int64](nil)
 
 		if s.ds == nil {
 			t.Fatal("Map didn't initialized")
 		}
 
-		s.ds["myGauge"] = *new(Gauge)
+		s.ds["myInt"] = new(int64)
 	})
-
-	t.Run("Create Counter Storage", func(t *testing.T) {
-		s := NewStorage[*Count](nil)
-
-		if s.ds == nil {
-			t.Fatal("Map didn't initialized")
-		}
-
-		s.ds["myCounter"] = new(Count)
-	})
-}
-
-func TestGet(t *testing.T) {
-	type want struct {
-		returnValue any
-		valueExist bool
-	}
-
-	storage := map[string]interface{Set(string, string) error; Get(string) (any, bool)}{
-		"gauge": NewStorage[Gauge](logger.LoggerAdapter(t.Log)),
-		"counter": NewStorage[*Count](logger.LoggerAdapter(t.Log)),
-	}
-
-	storage["gauge"].Set("myGauge", "1.1")
-	storage["counter"].Set("myCounter", "2")
-	tests := []struct {
-		name string
-		metricType string
-		metricName string
-		want want
-	}{
-		{
-			name: "Test Gauge that exist",
-			metricType: "gauge",
-			metricName: "myGauge",
-			want: want{returnValue: Gauge(1.1), valueExist: true},
-		},
-		{
-			name: "Test Gauge that not exist",
-			metricType: "gauge",
-			metricName: "testGauge",
-			want: want{returnValue: any(nil), valueExist: false},
-		},
-		{
-			name: "Test Counter that exist",
-			metricType: "counter",
-			metricName: "myCounter",
-			want: want{returnValue: Count(2), valueExist: true},
-		},
-		{
-			name: "Test Counter that not exist",
-			metricType: "counter",
-			metricName: "testCounter",
-			want: want{returnValue: any(nil), valueExist: false},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-
-			value, ok := storage[test.metricType].Get(test.metricName)
-
-
-			assert.Equal(t, test.want.valueExist, ok)
-
-			switch value.(type) {
-				case *Count:
-					assert.Equal(t, test.want.returnValue, *value.(*Count))
-				default:
-					assert.Equal(t, test.want.returnValue, value)
-			}
-		})
-	}
 }
 
 func TestSet(t *testing.T) {
+	storage := NewStorage[*int64](nil)
+
 	type want struct {
-		storedValue any
-		returnError bool
+		storedValue int64
 	}
 
-	storage := map[string]interface{Set(string, string) error; Get(string) (any, bool)}{
-		"gauge": NewStorage[Gauge](logger.LoggerAdapter(t.Log)),
-		"counter": NewStorage[*Count](logger.LoggerAdapter(t.Log)),
-	}
 	tests := []struct {
 		name string
-		metricType string
-		metricName string
-		metricValue string
+		itemName string
+		itemValue int64
 		want want
 	}{
 		{
-			name: "Test Gauge",
-			metricType: "gauge",
-			metricName: "testGauge",
-			metricValue: "100.0",
-			want: want{storedValue: Gauge(100.0)},
-		},
-		{
-			name: "Test Gauge with same metric name",
-			metricType: "gauge",
-			metricName: "testGauge",
-			metricValue: "110.0",
-			want: want{storedValue: Gauge(110.0)},
-		},
-		{
-			name: "Test Gauge with wrong type",
-			metricType: "gauge",
-			metricName: "testGauge",
-			metricValue: "sss",
-			want: want{storedValue: Gauge(110.0), returnError: true},
-		},
-		{
-			name: "Test Counter",
-			metricType: "counter",
-			metricName: "testCounter",
-			metricValue: "5",
-			want: want{storedValue: Count(5)},
-		},
-		{
-			name: "Test Counter with same metric name",
-			metricType: "counter",
-			metricName: "testCounter",
-			metricValue: "6",
-			want: want{storedValue: Count(11)},
-		},
-		{
-			name: "Test Counter with float type",
-			metricType: "counter",
-			metricName: "testCounter",
-			metricValue: "5.0",
-			want: want{storedValue: Count(11), returnError: true},
+			name: "Test Set myInt",
+			itemName: "myInt",
+			itemValue: int64(100),
+			want: want{storedValue: int64(100)},
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-
-			err := storage[test.metricType].Set(test.metricName, test.metricValue)
-			if test.want.returnError {
-				assert.Error(t, err)
-			} else {
-				assert.Equal(t, err, nil)
-			}
-
-			value, _ := storage[test.metricType].Get(test.metricName)
-
-			switch value.(type) {
-				case *Count:
-					assert.Equal(t, test.want.storedValue, *value.(*Count))
-				default:
-					assert.Equal(t, test.want.storedValue, value)
-			}
+			storage.Set(test.itemName, &test.itemValue)
+			value, _ := storage.Get(test.itemName)
+			assert.Equal(t, &test.want.storedValue, value)
 		})
 	}
+}
 
+func TestGet(t *testing.T) {
+	storage := NewStorage[*int64](nil)
+	myInt := int64(1)
+	storage.Set("myInt", &myInt)
 
-	// t.Fatal("not implemented")
+	type want struct {
+		returnValue *int64
+		valueExist bool
+	}
+
+	tests := []struct {
+		name string
+		itemName string
+		want want
+	}{
+		{
+			name: "Test Item that exist",
+			itemName: "myInt",
+			want: want{ returnValue: &myInt, valueExist: true },
+		},
+		{
+			name: "Test Item that not exist",
+			itemName: "testInt",
+			want: want{ returnValue: nil, valueExist: false },
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			value, ok := storage.Get(test.itemName)
+			assert.Equal(t, test.want.returnValue, value)
+			assert.Equal(t, test.want.valueExist, ok)
+		})
+	}
 }
 
 func TestRemove(t *testing.T) {
-	storage := map[string]interface{Set(string, string) error; Get(string) (any, bool); Remove(string)}{
-		"gauge": NewStorage[Gauge](logger.LoggerAdapter(t.Log)),
-		"counter": NewStorage[*Count](logger.LoggerAdapter(t.Log)),
-	}
+	storage := NewStorage[*int64](nil)
+	testInt := int64(2)
 
-	storage["gauge"].Set("testGauge", "2.1")
-	storage["counter"].Set("testCounter", "2")
+	storage.Set("testInt", &testInt)
 	tests := []struct {
 		name string
-		metricType string
-		metricName string
+		itemName string
 	}{
 		{
-			name: "Test Gauge Remove",
-			metricType: "gauge",
-			metricName: "testGauge",
-		},
-		{
-			name: "Test Counter remove",
-			metricType: "counter",
-			metricName: "testCounter",
+			name: "Test testInt Remove",
+			itemName: "testInt",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-
-			storage[test.metricType].Remove(test.metricName)
-			_, ok := storage[test.metricType].Get(test.metricName)
+			storage.Remove(test.itemName)
+			_, ok := storage.Get(test.itemName)
 			assert.Equal(t, false, ok)
 		})
 	}
 }
 
-func TestTick(t *testing.T) {
-	t.Run("Test Tick", func(t *testing.T) {
-		c := new(Count)
-		c.Tick(Count(2))
-		assert.Equal(t, Count(2), *c)
-	})
-}
-
-func TestIsAllowed(t *testing.T) {
-	t.Run("Gauge obj is allowed obj", func(t *testing.T) {
-		g := any(new(Gauge))
-
-		if _, ok := g.(interface{isAllowed()}); !ok {
-			t.Error("Gauge не реализует isAllowed()")
-		}
-	})
-	t.Run("Count pointer is allowed obj", func(t *testing.T) {
-		c := any(new(Count))
-
-		if _, ok := c.(interface{isAllowed()}); !ok {
-			t.Error("Count не реализует isAllowed()")
-		}
-	})
-}

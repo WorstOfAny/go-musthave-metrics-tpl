cfcf3f4e90c084f7341d850f126403051816a0a3
Ivan Tyurin <i.tyurin@voskhod.ru>
2026-02-20 01:02:27 +0300

    Инкремент 2
    This reverts commit 0a9092ba2fb422cecc5b7a7cb2cfbf02e6a81220.

---
+++ internal/client/main_test.go
@@ -0,0 +1,25 @@
+package client
+
+import(
+	"net/http/httptest"
+	"net/http"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewClient(t *testing.T) {
+	NewClient(nil)
+}
+
+func TestPost(t *testing.T) {
+	c := NewClient(nil)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "text/plain", r.Header.Get("Content-Type"))
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(""))
+	}))
+	defer ts.Close()	
+
+	c.Post(ts.URL)
+}

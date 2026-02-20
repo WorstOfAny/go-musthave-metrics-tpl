6963d1c968b98abda2b918a146b8e4717f57631e
Ivan Tyurin <i.tyurin@voskhod.ru>
2026-02-20 18:28:57 +0300

    Инкремент 6

--- internal/handler/main.go
+++ internal/handler/main.go
@@ -3,16 +3,38 @@ package handler
 import(
 	"github.com/WorstOfAny/go-musthave-metrics-tpl/internal/model"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	//"github.com/go-chi/chi/v5/middleware"
+	//"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"net/http"
 	"strings"
 	"context"
 	"fmt"
+	"time"
 )
 
 type metricsController struct {}
-
 type ctxKey string
+type responseData struct {
+	status int
+	size int
+}
+type responseWriter struct {
+	http.ResponseWriter
+	responseData *responseData
+}
+
+func (wr *responseWriter) Write(b []byte) (int, error) {
+	size, err := wr.ResponseWriter.Write(b)
+	wr.responseData.size += size
+	return size, err
+}
+
+func (wr *responseWriter) WriteHeader(statusCode int) {
+	wr.ResponseWriter.WriteHeader(statusCode)
+	wr.responseData.status = statusCode
+}
+
 const(
 	metricKey ctxKey = "metric"
 	metricTypeKey ctxKey = "metricType"
@@ -21,7 +43,7 @@ const(
 )
 
 func (c *metricsController) ApplyTo(mux chi.Router) {
-	mux.Use(middleware.Logger)
+	mux.Use(logRequest)
 	mux.Use(textPlainTypeSet)
 	mux.Get("/", c.listAll)
 	mux.Route("/value/{metricType}/{metricName}", func(r chi.Router) {
@@ -39,6 +61,26 @@ func (c *metricsController) ApplyTo(mux chi.Router) {
 
 func NewController() *metricsController {
 	return &metricsController{}
+}
+
+func logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		responseD := &responseData{status: http.StatusOK}
+		lw := responseWriter{ResponseWriter: w, responseData: responseD}
+
+		next.ServeHTTP(&lw, r)
+
+		log.Info().
+			Str("request_method", r.Method).
+			Str("request_uri", r.RequestURI).
+			Dur("duration", time.Since(start)).
+			Msg("")
+		log.Info().
+			Int("response_status", responseD.status).
+			Int("response_size", responseD.size).
+			Msg("")
+	})
 }
 
 func metricTypeCtx(next http.Handler) http.Handler {
